9826143ce2dbbf5a19758498ae76dd5b2a7b8e7d
Mariam Haider <mfh.haider@gmail.com>
2020-04-23 18:07:10 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,12 +2,7 @@ class Dog
   
   attr_accessor :id, :name, :breed
   
- def initialize(id=nil, name, breed)
-    @id = id
-    @name = name
-    @breed = breed
-  end
-  
+
   def self.create_table
      sql = <<-SQL
       CREATE TABLE IF NOT EXISTS dogs (
