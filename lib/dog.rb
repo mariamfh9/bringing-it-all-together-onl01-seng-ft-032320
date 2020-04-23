eaba500050834beba69b8b84929f4b1bb09a6952
Mariam Haider <mfh.haider@gmail.com>
2020-04-23 18:06:49 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,10 +2,11 @@ class Dog
   
   attr_accessor :id, :name, :breed
   
-  def initialize(attributes)
-   attributes.each {|key, value| self.send(("#{key}="), value)}
-    self.id ||= nil
-  end 
+ def initialize(id=nil, name, breed)
+    @id = id
+    @name = name
+    @breed = breed
+  end
   
   def self.create_table
      sql = <<-SQL
